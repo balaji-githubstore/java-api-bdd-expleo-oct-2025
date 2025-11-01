861e67a2686cde0f87e19fda9c57b581ebf6dbd5
balaji-githubstore <balajidinakaran1@gmail.com>
2025-11-01 13:41:17 +0530

    Added RequestSpecification for GitRequestSpecBuilder

---
+++ src/main/java/com/expleo/utils/GitRequestSpecBuilder.java
@@ -0,0 +1,38 @@
+package com.expleo.utils;
+
+
+import com.expleo.config.ConfigReader;
+import com.expleo.config.TokenManager;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class GitRequestSpecBuilder {
+
+    public static RequestSpecification getGitRequestSpec() {
+
+        return new RequestSpecBuilder()
+                .setBaseUri(ConfigReader
+                        .getBaseURI())
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", TokenManager.getToken())
+                .addFilter(OpenAPIValidatorUtils.getValidationFilter())
+                .log(LogDetail.ALL)
+                .build();
+
+    }
+
+    public static RequestSpecification getGitRequestSpecWithoutValidation() {
+
+        return new RequestSpecBuilder()
+                .setBaseUri(ConfigReader
+                        .getBaseURI())
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", TokenManager.getToken())
+                .log(LogDetail.ALL)
+                .build();
+
+    }
+
+}
